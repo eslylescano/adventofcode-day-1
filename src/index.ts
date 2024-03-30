9dca9b28df197303e56c60661a7d0ff041d1e676
Esly Lescano <eslylescano@gmail.com>
2024-03-30 16:48:16 +0100

    Add implementation to pass the tests

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 export function sumOfCalibrationValues(document: string[]): number {
     let sum = 0;
+    for (const line of document) {
+        const numbers = line.match(/\d+/g);
+        if (numbers !== null) {
+        const firstDigit = parseInt(numbers[0].charAt(0));
+        const lastDigit = parseInt(numbers[numbers.length - 1].charAt(0));
+        sum += (firstDigit * 10) + lastDigit;
+        }
+    }
+
     return sum;
   }
