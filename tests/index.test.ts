36408c8b1e5a8f3920b6670992f5cc02711bed9a
Esly Lescano <eslylescano@gmail.com>
2024-03-30 14:13:56 +0100

    Add the rest of the cases

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,34 @@
 import { sumOfCalibrationValues } from "../src";
 
 describe('sumOfCalibrationValues', () => {
-    it('returns the sum of calibration values for a single line', () => {
+    it('returns the sum of calibration values for a single line case 1', () => {
       const document = ['1abc2'];
       expect(sumOfCalibrationValues(document)).toBe(12);
     });
+
+    it('returns the sum of calibration values for a single line case 2', () => {
+        const document = ['pqr3stu8vwx'];
+        expect(sumOfCalibrationValues(document)).toBe(38);
+      });
+
+      it('returns the sum of calibration values for a single line case 3', () => {
+        const document = ['a1b2c3d4e5f'];
+        expect(sumOfCalibrationValues(document)).toBe(15);
+      });
+
+      it('returns the sum of calibration values for a single line case 4', () => {
+        const document = ['treb7uchet'];
+        expect(sumOfCalibrationValues(document)).toBe(77);
+      });
+
+    it('returns the sum of calibration values for multiple lines', () => {
+        const document = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet'];
+        expect(sumOfCalibrationValues(document)).toBe(142);
+      });
+    
+      it('handles empty input', () => {
+        const document: string[] = [];
+        expect(sumOfCalibrationValues(document)).toBe(0);
+      });
+    
   });
